6fc738a0b132cb5fd3aef46caf9dc3a36fa294e1
Fernando Abreu <fabreu@narwalsistemas.com.br>
2023-07-28 13:08:30 -0300

    Renomeando algumas classes

--- Infra/Repositorio/Generics/RepositoryGeneric.cs
+++ Infra/Repositorio/Generics/RepositoryGeneric.cs
@@ -1,37 +1,94 @@
 ﻿using Domain.Interfaces.Generics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Infra.Configuracao;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
 
 namespace Infra.Repositorio.Generics
 {
-    public class RepositoryGeneric<T> : InterfaceGeneric<T> where T : class
+    public class RepositoryGeneric<T> : InterfaceGeneric<T>, IDisposable where T : class
     {
-        public Task Add(T Objeto)
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositoryGeneric()
         {
-            throw new NotImplementedException();
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
         }
 
-        public Task Delete(T Objeto)
+        public async Task Add(T Objeto)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(Objeto);
+                await data.SaveChangesAsync();
+            }
         }
 
-        public Task<T> GetEntityById(int Id)
+        public async Task Delete(T Objeto)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Remove(Objeto);
+                await data.SaveChangesAsync();
+            }
         }
 
-        public Task<List<T>> List()
+        public async Task<T> GetEntityById(int Id)
         {
-            throw new NotImplementedException();
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(Id);
+            }
         }
 
-        public Task Update(T Objeto)
+        public async Task<List<T>> List()
         {
-            throw new NotImplementedException();
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().ToListAsync();
+            }
         }
+
+        public async Task Update(T Objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Update(Objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+
+        // Flag: Has Dispose already been called?
+        private bool disposed = false;
+
+        // Instantiate a SafeHandle instance.
+        private SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+
+        #endregion Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
     }
 }
