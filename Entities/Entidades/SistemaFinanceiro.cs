7f3297b594cda96a2cdc451b73c4af70d5f224a4
Fernando Abreu <fabreu@narwalsistemas.com.br>
2023-07-28 10:06:47 -0300

    Add project files.

---
+++ Entities/Entidades/SistemaFinanceiro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entidades
+{
+    [Table("SistemaFinanceiro")]
+    public class SistemaFinanceiro : Base
+    {
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+        public int DiaFechamento { get; set; }
+        public bool GerarCopiaDespesa { get; set; }
+        public bool MesCopia { get; set; }
+        public bool AnoCopia { get; set; }
+    }
+}
---
+++ Entities/Notificacoes/Notifica.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            notificacoes = new List<Notifica>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> notificacoes;
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigator}o",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigator}o",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+    }
+}
