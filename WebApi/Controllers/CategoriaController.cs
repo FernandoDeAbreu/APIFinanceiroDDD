88980f6739e149795596b9688c910c0180f7e39b
Fernando Abreu <fabreu@narwalsistemas.com.br>
2023-09-21 13:18:25 -0300

    Ajsutes

---
+++ WebApi/Controllers/CategoriaController.cs
@@ -0,0 +1,53 @@
+﻿using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.Services;
+using Entities.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoria _categoria;
+        private readonly ICategoriaServices _categoriaServices;
+
+        public CategoriaController(ICategoria categoria, ICategoriaServices categoriaServices)
+        {
+            _categoria = categoria;
+            _categoriaServices = categoriaServices;
+        }
+
+        [HttpGet("/api/ListarCategoriaUsuario")]
+        [Produces("application/json")]
+        public async Task<object> ListarCategoriaUsuario(string emailUsuario)
+        {
+            return _categoria.ListarCategoriaUsuario(emailUsuario);
+        }
+
+        [HttpPut("/api/AdicionarCategoria")]
+        [Produces("application/json")]
+        public async Task<object> AdicionarCategoria(Categoria categoria)
+        {
+            await _categoriaServices.AdicionarCategoria(categoria);
+            return categoria;
+        }
+
+        [HttpPut("/api/AtualizarCategoria")]
+        [Produces("application/json")]
+        public async Task<object> AtualizarCategoria(Categoria categoria)
+        {
+            await _categoriaServices.AtualizarCategoria(categoria);
+            return categoria;
+        }
+
+        [HttpGet("/api/ObterCategoria")]
+        [Produces("application/json")]
+        public async Task<object> ObterCategoria(int id)
+        {
+            var categoria = await _categoria.GetEntityById(id);
+            await _categoria.Delete(categoria);
+            return true;
+        }
+    }
+}
---
+++ WebApi/Controllers/DespesasController.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class DespesasController : ControllerBase
+    {
+        private readonly IDespesa _despesas;
+        private readonly IDespesaServices _despesaServices;
+        public DespesasController(IDespesaServices despesaServices, IDespesa despesas)
+        {
+            _despesaServices = despesaServices;
+            _despesas = despesas;
+        }
+    }
+}
